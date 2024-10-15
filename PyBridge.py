a50a736015f4c872784a794534abe4c95b1273dc
Willie169 <cg10250207@gmail.com>
2024-10-15 20:47:43 +0800

    init

---
+++ PyBridge.py
@@ -0,0 +1,78 @@
+class _this:
+    @property
+    def this(self):
+        return self
+
+class _operators:
+
+    def __add__():
+        return NotImplemented
+
+    operatorPlus = __add__
+    
+    def __sub__():
+        return NotImplemented
+
+    operatorMinus = __sub__
+
+    def __mul__():
+        return NotImplemented
+
+    operatorStar = __mul__
+
+    def __truediv__():
+        return NotImplemented
+
+    operatorDoubleSlash = __truediv__
+
+    def __floordiv__():
+        return NotImplemented
+
+    operatorSlash = __floordiv__
+
+    def __mod__():
+        return NotImplemented
+
+    operatorPercent = __mod__
+
+    def __pow__():
+        return NotImplemented
+
+    operatorDoubleStar = __pow__
+
+    def __eq__():
+        return NotImplemented
+
+    operatorEqual = __eq__
+
+    def __ne__():
+        return NotImplemented
+
+    operatorNotEqual = __ne__
+
+    def __lt__():
+        return NotImplemented
+
+    operatorLess = __lt__
+
+    def __le__():
+        return NotImplemented
+
+    operatorLessEqual = __le__
+
+    def __gt__():
+        return NotImplemented
+
+    operatorGreater = __gt__
+
+    def __ge__():
+        return NotImplemented
+
+    operatorGreaterEqual = __ge__
+
+class _scope:
+    def __enter__():
+        return
+
+    def __exit__():
+        return
